c8518477880af25d0f60dec2b9f2d017040364fe
mateoroca <mateojoserocaclemntis@gmail.com>
2023-05-18 16:01:35 -0300

    conection to mydb, create http server , config Environment Variables

--- access-control-module/API/src/app.js
+++ access-control-module/API/src/app.js
@@ -2,7 +2,35 @@ const { userData } = require("./models/UserData.js");
 const { AccesHandler } = require("./controllers/AccessHandler.js");
 const { Authorizer } = require("./controllers/Authorizer.js");
 const { UserHandler } = require("./controllers/UserHandler.js");
+const { connection } = require("./dataBase");
 
-let userHandler = new UserHandler();
+const http = require("http");
+require("dotenv").config();
 
-userHandler.read(userData);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end();
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server is lisening on port: ${port}`);
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error("Error to conect DB: ", error);
+  } else {
+    console.log("Susccess conocetion to DB!");
+  }
+});
+
+connection.query("SELECT * FROM user", (error, results, fields) => {
+  if (error) {
+    console.error("QUERY ERROR : ", error);
+  } else {
+    console.log("QUERY RESULTS: ", results);
+  }
+});
